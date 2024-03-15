a172066a072c8b9ccc50f68466d7e68cc8d95c7e
aduf <dufalmira@gmail.com>
2024-03-15 10:10:41 +0700

    menambahkan data yang akan di simpan di localStorage

--- js/User.js
+++ js/User.js
@@ -1,2 +1,11 @@
 //file ini digunakan untuk mengurus business logic
 //file ini digunakan untuk mengelola data seperti create, read, update, etc
+
+class User {
+  saveUser(userData) {
+    const newUser = {
+      id: Date.now(),
+      ...userData,
+    };
+  }
+}

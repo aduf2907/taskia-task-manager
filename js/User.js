02569d713c8658d7dfe3a25e084f5cbb8677ba1e
aduf <dufalmira@gmail.com>
2024-03-15 10:05:45 +0700

    first commit

---
+++ js/User.js
@@ -0,0 +1,2 @@
+//file ini digunakan untuk mengurus business logic
+//file ini digunakan untuk mengelola data seperti create, read, update, etc
---
+++ js/addUser.js
@@ -0,0 +1,1 @@
+//file ini untuk penghubung antara UI HTML dan model User
